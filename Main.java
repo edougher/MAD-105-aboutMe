b55c8d79788707e23bad2c13ccc1950a2aed6dd9
Aaron Dougher <aaron@Aarons-MacBook-Pro.local>
2017-08-29 19:35:42 -0500

    A little about myself.

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hello, my name is Erin");
+        System.out.println("I'm pretty new to programing");
+        System.out.println("My first programming class was Intro to C++ a year and a half ago.");
+        System.out.println("So far it has been challenging which is what I've ejoyed about it the most.");
+    }
+}
